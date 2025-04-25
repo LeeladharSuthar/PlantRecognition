3f993b675c2e0cf2a496e2f403a80c3730f8d39c
Leeladhar Suthar <74850805+LeeladharSuthar@users.noreply.github.com>
2025-04-25 13:26:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ from torchvision.models import ResNet18_Weights
 from PIL import Image
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
